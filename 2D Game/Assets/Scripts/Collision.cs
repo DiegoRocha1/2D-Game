5542c10f62a2abd6d7f4123b9151c4cc4020ea2f
Diego Rocha <diegorocha@email.arizona.edu>
2018-10-09 03:10:28 -0700

    Trying to add collision to particles.

---
+++ 2D Game/Assets/Scripts/Collision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update (Collider col) {
+		if(col.name == "Particle System")
+		{
+			Score.pointVal += 1;
+			// It is object tagged with TagB
+		}
+	}
+d
+}
